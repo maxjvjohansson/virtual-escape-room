707773f8be85fe79dc9ae093b5c2127a7ac22541
maxjvjohansson <max.jv.johansson@gmail.com>
2025-05-01 17:24:11 +0200

    Initial NextJs/TypeScript setup, also cleaned up boilersettings

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "The Haunted Escape",
+  description: "A virtual horror escape room game",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <section>
+      <h1>The Haunted Escape</h1>
+      <button>Enter</button>
+      <button>Instructions</button>
+    </section>
+  );
+}
