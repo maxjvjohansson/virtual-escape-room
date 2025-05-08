b06b2e9dfa833549fffbca0d5ff0002894abb0d0
TobiasAndren <tobiasandren@hotmail.com>
2025-05-08 14:44:03 +0200

    Add: Computer-puzzle styling

--- src/components/Puzzles/ComputerPuzzle/ComputerPuzzleScreen.tsx
+++ src/components/Puzzles/ComputerPuzzle/ComputerPuzzleScreen.tsx
@@ -24,19 +24,56 @@ export default function ComputerPuzzleScreen({
   };
 
   return (
-    <section>
-      <h2>Windows XP</h2>
-      <p>Password hint: {riddle?.question}</p>
+    <section className="flex items-center flex-col justify-center border-[#0161fc] border-2">
+      <div className="bg-[#0161fc] text-white px-4 py-2 text-sm font-bold w-full">
+        Log On to Windows
+      </div>
+      <div className="bg-gradient-to-r from-[#688ae4] via-[#9cb9f5] from-20% via-60% to-[#799be9] to-90% w-full h-30 flex items-center justify-center border-b-[#ef5b25] border-b-2">
+        <p className="text-white flex flex-col">
+          <span className="text-sm font-thin">Microsoft©</span>
+          <span className="text-4xl/6 flex font-semibold">
+            Windows
+            <span className="text-xl/1 text-[#ef5b25] font-bold">xp</span>
+          </span>
+          <span className="font-thin">Professional x64 Edition</span>
+        </p>
+      </div>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-5 items-center h-50 justify-center bg-[#ede9d8] w-full"
+      >
+        <div className="flex justify-between w-100 flex-col gap-2">
+          <div className="flex justify-between w-100">
+            <p className="self-start">User name:</p>
+            <p className="w-70 border-1 border-gray-400 pl-2 bg-white">
+              Michael Myers
+            </p>
+          </div>
+          <div className="flex justify-between w-100">
+            <label htmlFor="password" className="self-start">
+              Password:
+            </label>
+            <input
+              name="password"
+              type="text"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-1 border-gray-400 pl-2 w-70 bg-white"
+            />
+          </div>
+        </div>
+        <p className="text-center w-100 text-xs text-gray-700">
+          Password hint: <span className="italic">{riddle?.question}</span>
+        </p>
         {error && <p>{error}</p>}
-        <button type="submit">Log in</button>
+        <button
+          type="submit"
+          className="border-1 rounded-sm p-0.5 w-30 bg-white hover:border-blue-500 transition-all duration-150 hover:bg-gray-200"
+        >
+          OK
+        </button>
       </form>
     </section>
   );
