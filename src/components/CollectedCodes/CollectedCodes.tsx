14112a7c44b61eb1e7ad78cfe62fcd158e87ff3d
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-05-18 09:52:09 +0200

    added state variables from GameContext

--- src/components/CollectedCodes/CollectedCodes.tsx
+++ src/components/CollectedCodes/CollectedCodes.tsx
@@ -1,5 +1,14 @@
+import { useGameContext } from "@/lib/context/GameContext";
+
 export default function CollectedCodes() {
   const mockNotes = ["A3", "B6", "C7", "D2"];
+  const { state } = useGameContext();
+  const code = state.code;
+  const computerSolved = state.puzzles.computer;
+  const oddoutSolved = state.puzzles.oddOneOut;
+  const wordSolved = state.puzzles.word;
+  const paintingSolved = state.puzzles.painting;
+  console.log("YAAY" ,code.A);
 
   return (
     <ul className="flex gap-6 text-white text-xl md:text-4xl tracking-wide">
