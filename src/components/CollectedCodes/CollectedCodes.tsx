11bc1564e4254d47a9a412fde482768e3aedb433
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-05-18 10:10:12 +0200

    Uppdate: removed mockCode and console.logs

--- src/components/CollectedCodes/CollectedCodes.tsx
+++ src/components/CollectedCodes/CollectedCodes.tsx
@@ -1,20 +1,15 @@
 import { useGameContext } from "@/lib/context/GameContext";
 
 export default function CollectedCodes() {
-  const mockNotes = ["A3", "B6", "C7", "D2"];
   const { state } = useGameContext();
   const code = state.code;
   const computerSolved = state.puzzles.computer;
   const oddoutSolved = state.puzzles.oddOneOut;
   const wordSolved = state.puzzles.word;
   const paintingSolved = state.puzzles.painting;
-  console.log("YAAY" ,code.A);
 
   return (
     <ul className="flex gap-6 text-white text-xl md:text-4xl tracking-wide">
-      {mockNotes.map((note, idx) => (
-        <li key={idx}>{note}</li>
-      ))}
       {computerSolved ? ( <p>A{code.A}</p> ): (<></>)}
       {oddoutSolved ? ( <p>B{code.B}</p> ): (<></>)}
       {wordSolved ? ( <p>C{code.C}</p> ): (<></>)}
