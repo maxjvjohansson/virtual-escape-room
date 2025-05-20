f3b41fa0d07388c8117ed078a2ccf8e5632211db
TobiasAndren <tobiasandren@hotmail.com>
2025-05-20 11:24:22 +0200

    Update: Changed hint button text

--- src/components/Puzzles/ComputerPuzzle/ComputerPuzzleScreen.tsx
+++ src/components/Puzzles/ComputerPuzzle/ComputerPuzzleScreen.tsx
@@ -105,7 +105,7 @@ export default function ComputerPuzzleScreen({
                   }
                 }}
               >
-                New hint
+                {!hasReqHint ? "Hint" : "Hide hint"}
               </button>
             </div>
           </form>
