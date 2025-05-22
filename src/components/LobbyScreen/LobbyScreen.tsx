7b02fe29633c061f54a9420ed44ce5841b602fe2
maxjvjohansson <max.jv.johansson@gmail.com>
2025-05-22 17:11:56 +0200

    Update: Background mansion image and logo size

--- src/components/LobbyScreen/LobbyScreen.tsx
+++ src/components/LobbyScreen/LobbyScreen.tsx
@@ -4,7 +4,7 @@ export default function LobbyScreen() {
   return (
     <section
       className="min-h-screen w-full bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"
-      style={{ backgroundImage: "url('/images/haunted_mansion.png')" }}
+      style={{ backgroundImage: "url('/images/mansion_bg.png')" }}
     >
       <LobbyForm />
     </section>
