d680ef2d7dfc3ca8bc9c0b288794ea5979ff5be2
maxjvjohansson <max.jv.johansson@gmail.com>
2025-05-22 16:52:20 +0200

    Add: Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         <GameProvider>
           <JwtListener />
