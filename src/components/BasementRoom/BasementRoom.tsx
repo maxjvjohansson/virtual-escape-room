b8945b1c4fe296c220a6104f43aee181c021d1dc
TobiasAndren <tobiasandren@hotmail.com>
2025-05-23 14:46:14 +0200

    Update: Added a popup instruction for mobile users and adjusted mobile styling

--- src/components/BasementRoom/BasementRoom.tsx
+++ src/components/BasementRoom/BasementRoom.tsx
@@ -20,14 +20,59 @@ export default function BasementRoom() {
   const [showPainting, setShowPainting] = useState(false);
   const [showCodeLock, setShowCodeLock] = useState(false);
 
+  const [showSwipeHint, setShowSwipeHint] = useState(false);
+  const [isFadingOut, setIsFadingOut] = useState(false);
+
   const router = useRouter();
 
   useEffect(() => {
+    if (window.innerWidth <= 768) {
+      setShowSwipeHint(true);
+      setIsFadingOut(false);
+
+      const fadeTimer = setTimeout(() => {
+        setIsFadingOut(true);
+      }, 2000);
+
+      const hideTimer = setTimeout(() => {
+        setShowSwipeHint(false);
+        setIsFadingOut(false);
+      }, 3000);
+
+      return () => {
+        clearTimeout(fadeTimer);
+        clearTimeout(hideTimer);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
     console.log(state.code);
+    console.log(showSwipeHint);
   }, [state.code]);
 
   return (
     <section className="w-screen h-screen overflow-auto touch-pan-x touch-pan-y">
+      {showSwipeHint && (
+        <div
+          className={`fixed inset-0 z-50 flex flex-col justify-center items-center bg-opacity-10 p-4 transition-opacity duration-1000 ease-in-out ${
+            isFadingOut ? "opacity-0" : "opacity-100"
+          } pointer-events-none`}
+        >
+          <div className="bg-[#F9EFE5] rounded-lg p-4 max-w-xs text-center shadow-lg opacity-60 flex flex-col justify-center items-center gap-2">
+            <Image
+              src={"/images/swipe-icon.png"}
+              height={720}
+              width={720}
+              alt="Swipe icon"
+              className="w-[25%] self-center"
+            ></Image>
+            <p className="text-lg text-black">
+              Swipe left and right to find objects to interact with.
+            </p>
+          </div>
+        </div>
+      )}
       <div className="absolute top-8 z-10 left-17 md:left-[43.5%]">
         <Timer
           startedAt={state.startedAt}
@@ -39,7 +84,7 @@ export default function BasementRoom() {
         className="relative w-[300vw] h-[100vh] bg-cover bg-center bg-no-repeat md:w-screen md:h-screen"
         style={{ backgroundImage: "url('/images/basement_empty.png')" }}
       >
-        <div className="absolute w-[20%] top-[40%] left-[30%] md:w-[16%] md:top-[40%] md:left-[32%] hover-shake">
+        <div className="absolute w-[20%] top-[38%] left-[29%] md:w-[16%] md:top-[40%] md:left-[32%] hover-shake">
           <Button onClick={() => setShowComputer(true)}>
             <Image
               src={"/images/computer.png"}
@@ -49,7 +94,7 @@ export default function BasementRoom() {
             ></Image>
           </Button>
         </div>
-        <div className="absolute w-[15%] top-[55%] left-[61%] md:w-[13%] md:top-[58%] md:left-[60.5%] hover-shake">
+        <div className="absolute w-[15%] top-[55%] left-[59%] md:w-[13%] md:top-[58%] md:left-[60.5%] hover-shake">
           <Button onClick={() => setShowOddOut(true)}>
             <Image
               src={"/images/cards.png"}
@@ -59,7 +104,7 @@ export default function BasementRoom() {
             ></Image>
           </Button>
         </div>
-        <div className="absolute w-[9%] top-[13%] right-[5%] md:w-[7%] md:top-[1%] md:right-[9.5%] hover-shake">
+        <div className="absolute w-[8%] top-[11%] right-[8%] md:w-[7%] md:top-[1%] md:right-[9.5%] hover-shake">
           <Button onClick={() => setShowWordPuzzle(true)}>
             <Image
               src={"/images/book.png"}
@@ -69,7 +114,7 @@ export default function BasementRoom() {
             ></Image>
           </Button>
         </div>
-        <div className="absolute w-[17%] top-[17%] left-[43%] md:w-[15%] md:top-[5%] md:left-[44%] hover-shake">
+        <div className="absolute w-[17%] top-[15%] left-[43%] md:w-[15%] md:top-[5%] md:left-[44%] hover-shake">
           <Button onClick={() => setShowPainting(true)}>
             <Image
               src={"/images/painting_rectangular.png"}
@@ -79,7 +124,7 @@ export default function BasementRoom() {
             ></Image>
           </Button>
         </div>
-        <div className="absolute w-[9%] top-[48%] left-[17.5%] md:top-[49%] md:left-[21%] md:w-[7%] hover-shake">
+        <div className="absolute w-[9%] top-[48%] left-[20%] md:top-[49%] md:left-[21%] md:w-[7%] hover-shake">
           <Button onClick={() => setShowCodeLock(true)} className="border-none">
             <Image
               src="/images/lock.png"
@@ -89,7 +134,7 @@ export default function BasementRoom() {
             />
           </Button>
         </div>
-        <div className="absolute w-[13%] top-[24%] right-[1%] md:w-[10%] md:top-[18%] md:right-[6%] hover-shake">
+        <div className="absolute w-[12%] top-[23%] right-[5%] md:w-[10%] md:top-[18%] md:right-[6%] hover-shake">
           <Button className="fake-item">
             <Image
               src={"/images/statue.png"}
@@ -99,7 +144,7 @@ export default function BasementRoom() {
             ></Image>
           </Button>
         </div>
-        <div className="absolute w-[9%] bottom-[25%] right-[3.5%] md:w-[7%] md:bottom-[18%] md:right-[7.5%] hover-shake">
+        <div className="absolute w-[9%] bottom-[24%] right-[7%] md:w-[7%] md:bottom-[18%] md:right-[7.5%] hover-shake">
           <Button className="fake-item">
             <Image
               src={"/images/candle.png"}
@@ -109,13 +154,13 @@ export default function BasementRoom() {
             ></Image>
           </Button>
         </div>
-        <div className="absolute w-[19%] top-[11%] right-[19%] md:w-[14%] md:top-[1%] md:right-[23%] hover-shake">
+        <div className="absolute w-[19%] top-[9%] right-[20%] md:w-[14%] md:top-[1%] md:right-[23%] hover-shake">
           <Button className="fake-item">
             <Image
               src={"/images/painting_circle.png"}
               width={720}
               height={720}
-              alt="A stone statue"
+              alt="A circular painting"
             ></Image>
           </Button>
         </div>
